8dbfaec9c2790337ac649f02ad2da478e3471121
gregorytcarroll <gregscarroll@gmail.com>
2020-07-19 23:27:47 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,6 +5,7 @@ variable key_name {}
 variable s3_bucket {}
 variable iam_instance_profile {}
 
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = 80
@@ -16,11 +17,26 @@ variable "ssh_port" {
 
 variable "instance_count" {
   description = "Number of EC2 instances"
+# Insert desired number of instances
   default = 1
 }
 
 variable "my_ip" {
   description = "IP for locking down SSH Access"
-  default = <<insert IP>>
+# Insert your IP for SSH Access
+  default = "82.38.111.189/32"
 }
+
+
+variable "db_name" {
+  description = "The port the server will use for HTTP requests"
+# Replace with desired DB Name
+  default = "terraform-state-locks"
+}
+
+variable "s3_name" {
+# Replace with desired S3 Name
+  default = "terraform-state-storage-cotest"
+}
+
 
