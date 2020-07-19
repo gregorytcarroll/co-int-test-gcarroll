8e07a3283c869d423b29ae5d8f04ca91ccea3e7d
gregorytcarroll <gregscarroll@gmail.com>
2020-07-19 23:31:11 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,14 @@
-# Display ELB IP address
+# # Display ELB IP address
 
-output "elb_dns_name" {
-  value = "${aws_elb.co-elb.dns_name}"
+output "co-elb" {
+  value = module.webinfra.co-elb
+}
+
+output "asg_name" {
+  value = module.webinfra.co_asg
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
